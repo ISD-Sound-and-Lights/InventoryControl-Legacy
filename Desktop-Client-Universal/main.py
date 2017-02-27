483231cacf0f587c48d020487b22795734ce9e8a
DyingEcho <DyingEcho@users.noreply.github.com>
2017-02-27 10:05:27 +0100

    Tabs, not spaces!

--- Desktop-Client-Universal/main.py
+++ Desktop-Client-Universal/main.py
@@ -4,20 +4,20 @@ from tkinter.ttk import *
 
 
 class globalvars:
-    itemCount=1
-    next_free_id = 0
-    selected_id=0
+	itemCount=1
+	next_free_id = 0
+	selected_id=0
 var=globalvars()
 
 class Location:
-    def __init__(self, name, id):
-        self.name = name
-        self.id = id
+	def __init__(self, name, id):
+		self.name = name
+		self.id = id
 class Item:
-    locationid = -1
-    def __init__(self, name, id):
-        self.name = name
-        self.id = id
+	locationid = -1
+	def __init__(self, name, id):
+		self.name = name
+		self.id = id
 
 items = []
 locations = []
@@ -35,74 +35,74 @@ root.minsize(width=700, height=420)
 
 frame = Frame(root)
 def save():
-    print("Saving")
-    itemfile=open("items.csv", "w")
-    for item in items:
-        itemfile.write(item.name+",")
-        itemfile.write(str(item.id)+",")
-        itemfile.write(str(item.locationid)+"\n")
-    itemfile.close()
+	print("Saving")
+	itemfile=open("items.csv", "w")
+	for item in items:
+		itemfile.write(item.name+",")
+		itemfile.write(str(item.id)+",")
+		itemfile.write(str(item.locationid)+"\n")
+	itemfile.close()
 def savebind(event):
-    save()
+	save()
 def load():
-    itemfile=open("items.csv", "r")
-    rows=itemfile.read().split("\n")
-    itemfile.close()
-    for row in rows:
-        values=row.split(",")
-        if not values==['']:
-            items.append(Item(values[0],int(values[1])))
-            items[var.itemCount-1].locationid=values[2]
-    for item in items:
-        valid=False
-        trycount=0
-        addtext=""
-        while not valid:
-            try:
-                globalvars.next_free_id+=1
-                itemlist.insert("", var.itemCount, text=item.name + addtext,values=(item.id))
-            except TclError:
-                trycount+=1
-                addtext=str(trycount)
-            else:
-                var.itemCount+=1
-                valid=True
+	itemfile=open("items.csv", "r")
+	rows=itemfile.read().split("\n")
+	itemfile.close()
+	for row in rows:
+		values=row.split(",")
+		if not values==['']:
+			items.append(Item(values[0],int(values[1])))
+			items[var.itemCount-1].locationid=values[2]
+	for item in items:
+		valid=False
+		trycount=0
+		addtext=""
+		while not valid:
+			try:
+				globalvars.next_free_id+=1
+				itemlist.insert("", var.itemCount, text=item.name + addtext,values=(item.id))
+			except TclError:
+				trycount+=1
+				addtext=str(trycount)
+			else:
+				var.itemCount+=1
+				valid=True
 def updateItemList():
-    itemlist.delete(*itemlist.get_children())
-    for item in items:
-        itemlist.insert("", var.itemCount,text=item.name ,values=(item.id))
+	itemlist.delete(*itemlist.get_children())
+	for item in items:
+		itemlist.insert("", var.itemCount,text=item.name ,values=(item.id))
 def newItem(event):
-    items.append(Item("New Item", globalvars.next_free_id))
-    itemlist.insert("", var.itemCount, text=items[var.itemCount-1].name,values=(items[var.itemCount-1].id))
-    var.itemCount+=1
-    globalvars.next_free_id+=1
+	items.append(Item("New Item", globalvars.next_free_id))
+	itemlist.insert("", var.itemCount, text=items[var.itemCount-1].name,values=(items[var.itemCount-1].id))
+	var.itemCount+=1
+	globalvars.next_free_id+=1
 
 
 def getItemIndexById(identification):
-    ga = 0
-    for item in items:
-        if item.id == identification:
-            return ga
-        else:
-            ga += 1
+	ga = 0
+	for item in items:
+		if item.id == identification:
+			return ga
+		else:
+			ga += 1
 def getItemIndexByName(name):
-    ga=0
-    for item in items:
-        if(item.name==name):
-            return ga
-        else:
-            ga+=1
+	ga=0
+	for item in items:
+		if(item.name==name):
+			return ga
+		else:
+			ga+=1
 def select(event):
-    print("Selected Menu Item")
-    selected = itemlist.item(itemlist.selection()[0])["text"]
-    theItem = items[getItemIndexByName(selected)]
-    itemNameEntry.delete(0,"end")
-    itemNameEntry.insert(0,theItem.name)
-    var.selected_id=theItem.id
+	print("Selected Menu Item")
+	selected = itemlist.item(itemlist.selection()[0])["text"]
+	theItem = items[getItemIndexByName(selected)]
+	itemNameEntry.delete(0,"end")
+	itemNameEntry.insert(0,theItem.name)
+	var.selected_id=theItem.id
 def submit(event):
-    print(itemlist.item(itemlist.selection()))
-    items[getItemIndexById(var.selected_id)].name = itemNameEntry.get()
-    updateItemList()
+	print(itemlist.item(itemlist.selection()))
+	items[getItemIndexById(var.selected_id)].name = itemNameEntry.get()
+	updateItemList()
 
 
 #Begin UI Initialisation
